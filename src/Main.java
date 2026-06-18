bec8b3d97b3b088e7a44a088ac54d2c0b37425b0
Adithya20-afk <adithya.kp@s.amity.edu>
2026-06-18 14:05:19 +0530

    added main

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+/* 
+import controller.ExpenseController;
+
+public class Main {
+    public static void main(String[] args){
+        ExpenseController controller = new ExpenseController();
+        controller.start();
+    }
+}
+*/
+
+
+
+import db.DBConnection;
+import java.sql.Connection;
+
+public class Main{
+    public static void main(String[] args) {
+        try {
+            Connection con = DBConnection.getConnection();
+            System.out.println("Connected Successfully!");
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+/*
+import service.ExpenseService;
+import model.Expense;
+
+public class Main{
+    public static void main(String[] args) {
+
+        ExpenseService service = new ExpenseService();
+
+        boolean added = service.addExpenseDB(new Expense("Food", 250, "Pizza"));
+
+        System.out.println(added);
+    }
+}
+*/
+/*
+import java.util.ArrayList;
+
+import controller.ExpenseController;
+import model.Expense;
+import service.ExpenseService;
+
+public class Main {
+    public static void main(String[] args){
+        ExpenseService service = new ExpenseService();
+
+        ArrayList<Expense> expenses = service.getExpensesDB();
+
+        for (Expense e : expenses) {
+            System.out.println(e);
+        }
+    }
+}
+*/
+/*cd src
+java -cp ".;../lib/mysql-connector-j-9.7.0.jar" Main
+*/
+//To run from terminal
