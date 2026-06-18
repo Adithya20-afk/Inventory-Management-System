0a9f61bb6715726c641d960ff08f75bc29a9d3cb
Adithya20-afk <adithya.kp@s.amity.edu>
2026-06-18 13:34:33 +0530

    created service page

---
+++ service/ProductService.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import db.DBConnection;
+import model.Product;
+
+public boolean addProduct(Product product)
+
+public ArrayList<Product> getProducts()
+
+public Product getProductById(int id)
+
+public boolean updateQuantity(int id, int quantity)
+
+public boolean updatePrice(int id, double price)
+
+public boolean deleteProduct(int id)
+
+public ArrayList<Product> getLowStockProducts()
+
+public double getTotalInventoryValue()
